481dc1df9a149fb0e9492ee757b96605dbf415b0
Jakov Sosic <jsosic@gmail.com>
2016-10-18 23:59:05 +0200

    Install slack from repo

--- manifests/slack.pp
+++ manifests/slack.pp
@@ -3,10 +3,10 @@
 #
 class desktop::slack {
 
+  include ::yum::repo::slack
+
   package { 'slack':
     ensure   => present,
-    provider => 'rpm',
-    source   => 'https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-2.0.0-0.1.fc21.x86_64.rpm',
   }
 
 }
