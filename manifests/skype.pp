f523e842f5d7708daa0dcd9d75baa8c4628503a5
Jakov Sosic <jsosic@gmail.com>
2018-05-05 01:16:10 +0200

    Change skype RPM name

--- manifests/skype.pp
+++ manifests/skype.pp
@@ -1,9 +1,10 @@
+#
 # = Class: desktop::skype
 #
 # This module installs Skype
 #
 class desktop::skype {
 
-  package { 'skype': ensure => present, }
+  package { 'skypeforlinux': ensure => present, }
 
 }
